ff62e3bb32878fcfdedad81c828f09b13f3636f4
kashishdugar2017 <kashishdugar2017@gmail.com>
2026-09-09 11:51:11 +0530

    Fix Render allowed hosts

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2o3qa&c1w&2js0a61cn!jle6^fs=)2+kjtrqgf^!f@+3tt%3nc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["curio-log.onrender.com"]
 
 
 # Application definition
